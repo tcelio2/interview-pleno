a02c5582c168e3aa31014ebf18c101f3b6bb653e
Thiago Pereira Célio <thiago.pereira@adtsys.com.br>
2020-03-13 02:10:11 -0300

    criacao das classes de modelo

--- model/src/main/java/br/com/brainweb/interview/model/Hero.java
+++ model/src/main/java/br/com/brainweb/interview/model/Hero.java
@@ -1,4 +1,66 @@
 package br.com.brainweb.interview.model;
 
-public class Hero {
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name="hero")
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Hero  implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(columnDefinition = "serial")
+	private Long id;
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="race")
+	private String race;
+	
+	@Column(name="enabled")
+	private Boolean enabled;
+
+	@CreationTimestamp
+	@Column(name="created_at")
+	private Date createdAt;
+	
+	@UpdateTimestamp
+	@Column(name="updated_at")
+	private Date updatedAt;
+	
+	@ManyToOne
+	@JoinColumn(name="power_stats_id")
+	private PowerStats power;
+	
 }
--- model/src/main/java/br/com/brainweb/interview/model/PowerStats.java
+++ model/src/main/java/br/com/brainweb/interview/model/PowerStats.java
@@ -1,4 +1,67 @@
 package br.com.brainweb.interview.model;
 
-public class PowerStats {
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Entity
+@Table(name="power_stats")
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PowerStats implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(columnDefinition = "serial")
+	private Long id;
+	
+	@Column(name="strength")
+	private Integer strength;
+	
+	@Column(name="agility")
+	private Integer agility;	
+	
+	@Column(name="dexterity")
+	private Integer dexteriry;
+	
+	@Column(name="intelligence")
+	private Integer intelligence;
+	
+	@CreationTimestamp
+	@Column(name="created_at")
+	private Date createdAt;
+	
+	@UpdateTimestamp
+	@Column(name="updated_at")
+	private Date updatedAt;
+//	@OneToMany(mappedBy="power")
+//	private Set<Hero> heroList;
+	
 }
