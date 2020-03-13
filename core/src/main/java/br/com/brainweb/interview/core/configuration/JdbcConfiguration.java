74ff1713cd5f80f26e7c9e8c5b5c94d0cef8f30d
Thiago Pereira Célio <thiago.pereira@adtsys.com.br>
2020-03-13 02:08:59 -0300

    docker + docker-compose + configuracao + properties

--- core/src/main/java/br/com/brainweb/interview/core/configuration/JdbcConfiguration.java
+++ core/src/main/java/br/com/brainweb/interview/core/configuration/JdbcConfiguration.java
@@ -1,47 +1,70 @@
 package br.com.brainweb.interview.core.configuration;
 
-import com.zaxxer.hikari.HikariDataSource;
+import java.util.concurrent.TimeUnit;
+
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 
-import javax.sql.DataSource;
-import java.util.concurrent.TimeUnit;
+import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
+@EnableAutoConfiguration
+@EnableJpaRepositories
+//@EnableAutoConfiguration(
+//exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class JdbcConfiguration {
 
-    @Value("${jdbc.url}")
-    private String jdbcUrl;
-
-    @Value("${jdbc.username}")
-    private String username;
-
-    @Value("${jdbc.password}")
-    private String password;
-
-    @Value("${jdbc.schema}")
-    private String schema;
-
+//    //@Value("${spring.datasource.url:jdbc:postgresql://localhost:5432/postgres}")
+//	//@Value("#{systemProperties['spring.datasource.url'] ?: 'jdbc:postgresql://localhost:5432/postgres'}")
+//    @Value("${spring.datasource.url}")
+//    private String jdbcUrl;
+//
+//    //@Value("${spring.datasource.username:brainweb}")
+//	//@Value("#{systemProperties['spring.datasource.username'] ?: 'brainweb'}")
+//    @Value("${spring.datasource.username}")
+//    private String username;
+//
+//    //@Value("${spring.datasource.password:brainweb}")
+//	//@Value("#{systemProperties['spring.datasource.password'] ?: 'brainweb'}")
+//    @Value("${spring.datasource.password}")
+//    private String password;
+//
+//    //@Value("${spring.datasource.schema:interview_service}")
+//	//@Value("#{systemProperties['spring.datasource.schema'] ?: 'interview_service'}")
+//    @Value("${spring.datasource.schema}")
+//    private String schema;
+//
+//    //@Autowired
+//    //private Environment env;
+    
     @Bean
     public DataSource dataSource() {
-
+    	
         HikariDataSource dataSource = new HikariDataSource();
 
         dataSource.setDriverClassName(org.postgresql.Driver.class.getName());
-        dataSource.setJdbcUrl(jdbcUrl);
-        dataSource.setUsername(username);
-        dataSource.setPassword(password);
+        dataSource.setJdbcUrl("jdbc:postgresql://localhost:5432/heros");
+        dataSource.setUsername("brainweb");
+        dataSource.setPassword("brainweb");
         dataSource.setMaximumPoolSize(getMaxPoolSize());
         dataSource.setConnectionTimeout(TimeUnit.SECONDS.toMillis(5L));
-        dataSource.setSchema(schema);
+        dataSource.setSchema("interview_service");
+        
         return dataSource;
     }
 
-    @Bean
+    @Bean(name="transactionManager")
     public PlatformTransactionManager platformTransactionManager() {
         return new DataSourceTransactionManager(dataSource());
     }
@@ -50,6 +73,11 @@ public class JdbcConfiguration {
     public NamedParameterJdbcTemplate namedParameterJdbcTemplate() {
         return new NamedParameterJdbcTemplate(dataSource());
     }
+    
+   /* @Bean
+    public FlywayMigrationInitializer flywayInitializer(Flyway flyway) {
+    	return new FlywayMigrationInitializer(flyway, (f) -> {});
+    }*/
 
     /**
      * Identifies how many connections can be opened based on Postgres recommended formula.
