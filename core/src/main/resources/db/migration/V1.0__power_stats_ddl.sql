7e5efe46f24a8736416dc1b33d82b66586abb04a
Thiago Pereira Célio <thiago.pereira@adtsys.com.br>
2020-03-13 02:06:29 -0300

    alteracoes feitas nos scripts SQL para retirar padrao uuid

--- core/src/main/resources/db/migration/V1.0__power_stats_ddl.sql
+++ core/src/main/resources/db/migration/V1.0__power_stats_ddl.sql
@@ -1,10 +1,10 @@
 CREATE TABLE power_stats
 (
-    id           UUID PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
-    strength     SMALLINT         NOT NULL,
-    agility      SMALLINT         NOT NULL,
-    dexterity    SMALLINT         NOT NULL,
-    intelligence SMALLINT         NOT NULL,
-    created_at   TIMESTAMPTZ      NOT NULL DEFAULT now(),
-    updated_at   TIMESTAMPTZ      NOT NULL DEFAULT now()
+    id           SERIAL PRIMARY KEY NOT NULL,
+    strength     SMALLINT           NOT NULL,
+    agility      SMALLINT           NOT NULL,
+    dexterity    SMALLINT           NOT NULL,
+    intelligence SMALLINT           NOT NULL,
+    created_at   TIMESTAMPTZ        NOT NULL DEFAULT now(),
+    updated_at   TIMESTAMPTZ        NOT NULL DEFAULT now()
 );
